51c45d2e45555cf02f578cdd19da583557fe6543
ee <e.ekmecic@gmail.com>
2017-06-02 14:18:20 -0400

    [chore] activate more lints
    Get the compiler to stop us from doing dumb things.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! A friendly Rust interface to the BeagleBone family of devices
+
+#![deny(bad_style,
+        unsafe_code,
+        const_err,
+        dead_code,
+        extra_requirement_in_impl,
+        improper_ctypes,
+        legacy_directory_ownership,
+        non_shorthand_field_patterns,
+        no_mangle_generic_items,
+        overflowing_literals,
+        path_statements,
+        patterns_in_fns_without_body,
+        plugin_as_library,
+        private_in_public,
+        private_no_mangle_fns,
+        private_no_mangle_statics,
+        safe_extern_statics,
+        unconditional_recursion,
+        unions_with_drop_fields,
+        unused,
+        unused_allocation,
+        unused_parens,
+        while_true,
+        missing_debug_implementations,
+        missing_docs,
+        trivial_casts,
+        trivial_numeric_casts,
+        unused_extern_crates,
+        unused_import_braces,
+        unused_qualifications,
+        unused_results)]
+
 #[cfg(test)]
 mod tests {
     #[test]
