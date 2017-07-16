9ae1b3d792621146ec06dc7f388be22406b2394b
ee <e.ekmecic@gmail.com>
2017-07-16 18:14:12 -0400

    [docs] add to and improve I2C and UART docs

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,4 +1,13 @@
 //! The I2C module.
+//!
+//! The BeagleBone Black has 2 I2C interfaces available (I2C1 and I2C2).
+//! These can be enabled by using the bone_capemgr module.
+//!
+//! As an example, use the following command to enable UART4:
+//! `sudo sh -c "echo 'BB-I2C1' > /sys/devices/platform/bone_capemgr/slots"`
+//! This command works for recent kernel versions.
+//! If you wish to enable another I2C, substitute its number for 1 in the
+//! command above.
 
 use errors::*;
 use std::fs::{File, OpenOptions};
@@ -32,7 +41,7 @@ struct i2c_ioctl_data {
 ioctl!(ioctl_set_i2c_slave_addr with I2C_SLAVE);
 ioctl!(ioctl_i2c_smbus with I2C_SMBUS);
 
-/// I2C device
+/// Represents and I2C interface.
 #[derive(Debug)]
 pub struct I2C {
   i2c_num: u8,
@@ -41,42 +50,114 @@ pub struct I2C {
 
 impl I2C {
   /// Creates a new I2C device.
-  pub fn new(m_i2c_num: u8) -> Result<(I2C)> {
-    let i2c_file_path = format!("/dev/i2c-{}", m_i2c_num);
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new I2C interface using BB_I2C1.
+  /// // Don't forget to enable the I2C beforehand using bone_capemgr.
+  /// // Consult the module documentation for more information!
+  /// let i2c = I2C::new(1).unwrap();
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// Method fails if `i2c_num` is an invalid I2C port (i.e. isn't within 1-2)
+  /// or if the kernel fails to open the port for some other reason.
+  pub fn new(i2c_num: u8) -> Result<(I2C)> {
+    let i2c_file_path = format!("/dev/i2c-{}", i2c_num);
     Ok(I2C {
-         i2c_num: m_i2c_num,
+         i2c_num: i2c_num,
          i2c_file: OpenOptions::new()
            .read(true)
            .write(true)
            .open(i2c_file_path)
-           .chain_err(|| "Unable to create new I2C device.")?,
+           .chain_err(|| format!("Failed to create new I2C #{}.", i2c_num))?,
        })
   }
 
+  /// Sets the address of the I2C slave device.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new I2C interface using BB_I2C1.
+  /// let i2c = I2C::new(1).unwrap();
+  ///
+  /// // Set the slave address to 0x45.
+  /// i2c.set_slave_address(0x45).unwrap();
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// Fails if the kernel is unable to set the slave device address to the
+  /// chosen value.
+  pub fn set_slave_address(&self, slave_addr: u16) -> Result<(i32)> {
+    unsafe {
+      Ok(ioctl_set_i2c_slave_addr(self.i2c_file.as_raw_fd(), slave_addr as *mut u8)
+           .chain_err(|| format!("Failed to set I2C slave device address to {}.", slave_addr))?)
+    }
+  }
+
   /// Writes a single byte to an I2C slave.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new I2C interface using BB_I2C1.
+  /// let i2c = I2C::new(1).unwrap();
+  ///
+  /// // Set the slave address to 0x45.
+  /// i2c.set_slave_address(0x45).unwrap();
+  ///
+  /// // Write a 1 to the I2C slave
+  /// i2c.write(1).unwrap();
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// Fails if the kernel is unable to write the chosen value to the device.
   pub fn write(&self, data: u8) -> Result<(i32)> {
     unsafe {
       Ok(self.i2c_call(0 /* 0 => write */, data, 1, ::std::ptr::null_mut())
-             .chain_err(|| "ll")?)
+             .chain_err(|| format!("Failed to write {} to the I2C device.", data))?)
     }
   }
 
   /// Reads a single byte from an I2C slave and returns it.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new I2C interface using BB_I2C1.
+  /// let mut i2c = I2C::new(1).unwrap();
+  ///
+  /// // Set the slave address to 0x45.
+  /// i2c.set_slave_address(0x45).unwrap();
+  ///
+  /// // Read some data from the I2C device and display it.
+  /// println!("Read {} from the I2C slave!", i2c.read().unwrap());
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// Fails if the kernel is unable to read from the device.
   pub fn read(&mut self) -> Result<(u8)> {
     let mut data = i2c_data { block: unsafe { mem::zeroed() } };
     unsafe {
       // TODO: fix this ugly temporary hack to avoid the "unused result" warning
-      let _ = self.i2c_call(1, 0, 1, &mut data).chain_err(|| "")?;
+      let _ = self.i2c_call(1, 0, 1, &mut data)
+                  .chain_err(|| "Failed to read from the I2C device.")?;
     };
     Ok(data.block[0])
-  }
-
-  /// sets the addr of the slave
-  pub fn set_slave_address(&self, slave_addr: u16) -> Result<(i32)> {
-    unsafe {
-      Ok(ioctl_set_i2c_slave_addr(self.i2c_file.as_raw_fd(), slave_addr as *mut u8)
-           .chain_err(|| "unable to set slave device address.")?)
-    }
   }
 
   /// Wrapper function around the IOCTL FFI that does all of the IOCTL dirty
--- src/uart.rs
+++ src/uart.rs
@@ -1,4 +1,18 @@
 //! The UART module.
+//!
+//! The BeagleBone Black has 6 UART interfaces available (UART{0-5}).
+//! UART0 is enabled by default and is used by the kernel.
+//! The rest of the UART devices can be enabled by using the bone_capemgr
+//! module.
+//!
+//! As an example, use the following command to enable UART4:
+//! `sudo sh -c "echo 'BB-UART4' > /sys/devices/platform/bone_capemgr/slots"`
+//! This command works for recent kernel versions.
+//! If you wish to enable another UART, substitute its number for 4 in the
+//! command above.
+//!
+//! This is currently a simple wrapper around the `serialport` library due to
+//! time constraints.
 
 use errors::*;
 use serialport::open;
@@ -13,23 +27,71 @@ pub struct UART {
 
 impl UART {
   /// Creates a new UART port.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new UART using BB_UART2.
+  /// // Don't forget to enable the UART beforehand using bone_capemgr.
+  /// // Consult the module documentation for more information!
+  /// let uart = UART::new(2).unwrap();
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// Method fails if `uart_num` is an invalid UART port (i.e. isn't within 0-5)
+  /// or if the kernel fails to open the port for some other reason.
   pub fn new(uart_num: u32) -> Result<(UART)> {
     let port_path = format!("/dev/ttyO{}", uart_num);
     Ok(UART {
          port: open(&port_path)
-           .chain_err(|| "Unable to open UART port")?,
+           .chain_err(|| format!("Failed to open UART port #{}.", uart_num))?,
        })
   }
 
+
   /// Write data to a UART port.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new UART using BB_UART2.
+  /// let mut uart = UART::new(2).unwrap();
+  ///
+  /// // Write "hello!" to the UART port.
+  /// uart.write("hello!").unwrap();
+  /// ```
+  ///
+  /// # Errors
+  ///
+  /// Method fails if the kernel rejects outgoing data for some reason.
   pub fn write(&mut self, data: &str) -> Result<()> {
     self.port
         .write_all(data.as_bytes())
-        .chain_err(|| "Failed to write to UART port")?;
+        .chain_err(|| "Failed to write to UART port.")?;
     Ok(())
   }
 
   /// Read the specified number of bytes from the UART port.
+  ///
+  /// Returns a vector of bytes containing the bytes that were read from the
+  /// port.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new UART using BB_UART2.
+  /// let mut uart = UART::new(2).unwrap();
+  ///
+  /// // Read 10 bytes from the UART port.
+  /// uart.read_chars(10).unwrap();
+  /// ```
   pub fn read_chars(&mut self, num_bytes: usize) -> Result<(Vec<u8>)> {
     let mut buf: Vec<u8> = Vec::with_capacity(num_bytes);
 
@@ -41,6 +103,22 @@ impl UART {
   }
 
   /// Read the specified number of bytes and return it as a string.
+  ///
+  /// Returns a string containing the bytes that were read from the port.
+  /// Just a thin wrapper around `read_chars` to save the trouble of converting
+  /// it yourself.
+  ///
+  /// # Examples
+  ///
+  /// ```no_run
+  /// use libbeaglebone::prelude::*;
+  ///
+  /// // Create a new UART using BB_UART2.
+  /// let mut uart = UART::new(2).unwrap();
+  ///
+  /// // Read 10 bytes from the UART port.
+  /// uart.read_to_string(10).unwrap();
+  /// ```
   pub fn read_to_string(&mut self, num_bytes: usize) -> Result<(String)> {
     let mut buf: Vec<u8> = Vec::with_capacity(num_bytes);
 
@@ -92,41 +170,41 @@ impl UART {
   pub fn set_baud_rate(&mut self, baud_rate: BaudRate) -> Result<()> {
     Ok(self.port
            .set_baud_rate(baud_rate)
-           .chain_err(|| "Unable to set UART baud rate.")?)
+           .chain_err(|| "Failed to set UART baud rate.")?)
   }
 
   /// Set the data bits on the UART port.
   pub fn set_data_bits(&mut self, data_bits: DataBits) -> Result<()> {
     Ok(self.port
            .set_data_bits(data_bits)
-           .chain_err(|| "Unable to set UART data bits.")?)
+           .chain_err(|| "Failed to set UART data bits.")?)
   }
 
   /// Set the flow control on the UART port.
   pub fn set_flow_control(&mut self, flow_control: FlowControl) -> Result<()> {
     Ok(self.port
            .set_flow_control(flow_control)
-           .chain_err(|| "Unable to set UART flow control.")?)
+           .chain_err(|| "Failed to set UART flow control.")?)
   }
 
   /// Set the parity on the UART port.
   pub fn set_parity(&mut self, parity: Parity) -> Result<()> {
     Ok(self.port
            .set_parity(parity)
-           .chain_err(|| "Unable to set UART parity.")?)
+           .chain_err(|| "Failed to set UART parity.")?)
   }
 
   /// Set the stop bits on the UART port.
   pub fn set_stop_bits(&mut self, stop_bits: StopBits) -> Result<()> {
     Ok(self.port
            .set_stop_bits(stop_bits)
-           .chain_err(|| "Unable to set UART stop bits.")?)
+           .chain_err(|| "Failed to set UART stop bits.")?)
   }
 
   /// Set the timeout on the UART port.
   pub fn set_timeout(&mut self, timeout: Duration) -> Result<()> {
     Ok(self.port
            .set_timeout(timeout)
-           .chain_err(|| "Unable to set UART timeout.")?)
+           .chain_err(|| "Failed to set UART timeout.")?)
   }
 }
